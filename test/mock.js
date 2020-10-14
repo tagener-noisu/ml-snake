41de66d3a093f911e4963a4af7f8600761d5ef56
tagener-noisu <op4.renegat@gmail.com>
2020-10-14 11:55:35 +0600

    Extract Mock into new file

---
+++ test/mock.js
@@ -0,0 +1,33 @@
+class Mock {
+    constructor() {
+        this.call_num = 0;
+        this.expected_calls = [];
+        this.return_values = [];
+        this.calls = [];
+    }
+
+    expect_call(method, args, return_value = undefined) {
+        const call = {};
+        call[method] = args;
+
+        this.expected_calls.push(call);
+        this.return_values.push(return_value);
+    }
+
+    register_call(method, args) {
+        const call = {};
+        call[method] = [...args];
+
+        this.calls.push(call);
+
+        const result = this.return_values[this.call_num];
+        this.call_num = this.call_num + 1;
+        return result;
+    }
+
+    verify() {
+        expect(this.calls).toEqual(this.expected_calls);
+    }
+}
+
+module.exports = Mock
--- test/mocks.js
+++ test/mocks.js
@@ -1,34 +1,4 @@
-class Mock {
-    constructor() {
-        this.call_num = 0;
-        this.expected_calls = [];
-        this.return_values = [];
-        this.calls = [];
-    }
-
-    expect_call(method, args, return_value = undefined) {
-        const call = {};
-        call[method] = args;
-
-        this.expected_calls.push(call);
-        this.return_values.push(return_value);
-    }
-
-    register_call(method, args) {
-        const call = {};
-        call[method] = [...args];
-
-        this.calls.push(call);
-
-        const result = this.return_values[this.call_num];
-        this.call_num = this.call_num + 1;
-        return result;
-    }
-
-    verify() {
-        expect(this.calls).toEqual(this.expected_calls);
-    }
-}
+const Mock = require("./mock");
 
 class SnakeMock extends Mock {
     constructor(head) {
