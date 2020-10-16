502198a5516bcb473e1b9c753fa2d3c7bec05bbd
tagener-noisu <op4.renegat@gmail.com>
2020-10-16 11:48:07 +0600

    Add CellType enum

--- test/game_test.js
+++ test/game_test.js
@@ -428,33 +428,40 @@ describe("GameBoard", () => {
     const size = new Vector2D(3, 3);
     const pos = new Vector2D(0, 0);
 
+    it("has cell type enum", () => {
+        expect(GameBoard.CellType.OutOfBounds).toBe(0);
+        expect(GameBoard.CellType.Empty).toBe(1);
+        expect(GameBoard.CellType.Snake).toBe(2);
+        expect(GameBoard.CellType.Food).toBe(3);
+    });
+
     it("checks empty cells", () => {
         const board = new GameBoard(size);
 
-        expect(board.cell(pos)).toEqual("empty");
+        expect(board.cell(pos)).toEqual(GameBoard.CellType.Empty);
     });
 
     it('checks "out of bound" cells', () => {
         const board = new GameBoard(size);
 
-        expect(board.cell(new Vector2D(0, 3))).toEqual("out of bound");
-        expect(board.cell(new Vector2D(3, 0))).toEqual("out of bound");
-        expect(board.cell(new Vector2D(-1, 0))).toEqual("out of bound");
-        expect(board.cell(new Vector2D(0, -2))).toEqual("out of bound");
+        expect(board.cell(new Vector2D(0, 3))).toEqual(GameBoard.CellType.OutOfBounds);
+        expect(board.cell(new Vector2D(3, 0))).toEqual(GameBoard.CellType.OutOfBounds);
+        expect(board.cell(new Vector2D(-1, 0))).toEqual(GameBoard.CellType.OutOfBounds);
+        expect(board.cell(new Vector2D(0, -2))).toEqual(GameBoard.CellType.OutOfBounds);
     });
 
     it("checks food cells", () => {
         const board = new GameBoard(size);
         board.set_food(pos);
 
-        expect(board.cell(new Vector2D(0, 0))).toEqual("food");
+        expect(board.cell(new Vector2D(0, 0))).toEqual(GameBoard.CellType.Food);
     });
 
     it("checks snake cells", () => {
         const board = new GameBoard(size);
         board.set_snake(pos);
 
-        expect(board.cell(new Vector2D(0, 0))).toEqual("snake");
+        expect(board.cell(new Vector2D(0, 0))).toEqual(GameBoard.CellType.Snake);
     });
 
     it('it doesn\'t "store out of bounds" cells', () => {
