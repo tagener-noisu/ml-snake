78059296235e055db98e41e77ce3dacde44e0cd4
tagener-noisu <op4.renegat@gmail.com>
2020-11-12 12:47:48 +0600

    Compute distance between two points

--- test/vector2d_test.js
+++ test/vector2d_test.js
@@ -41,4 +41,11 @@ describe("Vector2D", () => {
 
         expect(result.equals(new Vector2D(1, 0))).toBe(true);
     });
+
+    it("computes distance", () => {
+        const one = new Vector2D(0, 0);
+        const two = new Vector2D(2, 0);
+
+        expect(one.distance(two)).toBeCloseTo(2.0)
+    });
 });
